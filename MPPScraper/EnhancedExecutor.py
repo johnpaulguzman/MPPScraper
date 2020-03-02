d948522f095fc692c2233891edbb8f04a202ec90
John Paul Guzman <jpguzman@viyahe.com>
2020-03-02 20:18:41 +0800

    tempscraper

--- MPPScraper/EnhancedExecutor.py
+++ MPPScraper/EnhancedExecutor.py
@@ -1,11 +1,24 @@
+import multiprocessing_utils
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.proxy import Proxy, ProxyType
+
 import itertools
 import multiprocessing as mp
-import os
 import time
 
 from Config import Config
-from BaseExecutor import BaseExecutor
 from ProxyService import ProxyService
+
+
+thread_local = multiprocessing_utils.local()
+
+class CommandContext:
+    def __init__(self, initialize, get_context, execution_plan):
+        self.initialize = initialize
+        self.get_context = get_context
+        self.execution_plan = execution_plan
 
 class CommandTemplate:
     def __init__(self, template, *var_generators, nested=False):
@@ -17,61 +30,100 @@ class CommandTemplate:
         var_combinations = itertools.product(*self.var_generators)
         if self.nested:  # flatten the stream via itertools.chain.from_iterable
             var_combinations = map(itertools.chain.from_iterable, var_combinations)
-        commands = itertools.starmap(self.template, var_combinations)
-        commands = tuple(commands)
-        return commands
+        args = itertools.starmap(self.template, var_combinations)
+        args = tuple(args)
+        return args
 
-class CommandEnhancer:
-    def __init__(self, initializer, decorator):
-        self.initializer = initializer
-        self.decorator = decorator
-
-class EnhancedExecutor(BaseExecutor):
-    def __init__(self, command_template, command_enhancer, workers=1):
-        self.commands = command_template.build()
-        self.command_enhancer = command_enhancer
+class ContextExecutor():
+    def __init__(self, arg_template, command_context, workers=1):
+        self.args = arg_template.build()
+        self.command_context = command_context
         self.workers = workers
 
-    def run_commands_split(self, command, mp_dict):
-        pid = os.getpid()
-        if mp_dict.get(pid) is None:
-            mp_dict[pid] = self.command_enhancer.initializer()
+    def run_commands_split(self, arg):
+        driver = self.command_context.get_context()
         for i in range(Config.max_attempts):
-            enhanced_command = self.command_enhancer.decorator(command, mp_dict[pid])
-            print(f">> Running [{pid}] attempt#[{i}] with processes [{enhanced_command}].")
-            output = self.run_command(enhanced_command)
-            if output.get('returncode', None) == 0:
+            output = self.command_context.execution_plan(driver, arg)
+            if output.get('succeeded', False):
                 break
-            print(f"** Command failed. Reinitializing mp cache...")
-            mp_dict[pid] = self.command_enhancer.initializer()
-        print(f"<< Finished [{pid}] attempt#{i}/{Config.max_attempts} with processes [{enhanced_command}].")
+            print(f"** Command failed. Running with a new context...")
+            driver = self.command_context.initialize()
+        print(f"<< Finished attempt#{i}/{Config.max_attempts} with processes [{arg}].")
         return output
 
     def run_commands_on_workers(self):
+        self.run_commands_split(self.args[0]) ######
         start_time = time.time()
-        with mp.Pool(processes=self.workers) as pool, mp.Manager() as manager:
-            mp_dict = manager.dict()
-            print(f">> Multiprocessing pool {(self.workers, pool, mp_dict)} loaded.")
-            starargs = ((command, mp_dict) for command in self.commands)
-            results = pool.starmap(self.run_commands_split, starargs)
-            print(f"<< Multiprocessing pool {(self.workers, pool, mp_dict)} unloaded.")
+        with mp.Pool(processes=self.workers) as pool:
+            print(f">> Multiprocessing pool {(self.workers, pool)} loaded.")
+            results = pool.map(self.run_commands_split, self.args)
+            print(f"<< Multiprocessing pool {(self.workers, pool)} unloaded.")
         print(f"Duration on workers = {time.time() - start_time}")
         return results
 
+###########################################################
+###########################################################
+###########################################################
 
-if __name__ == '__main__':
-    proxy_service = ProxyService()
-    initializer = proxy_service.generate_proxy
-    def decorator(c, p):  # multiprocessing hates lambdas
-        return f"{c} --proxy {p}" if p is not None else c
-    curl_proxy_decorator = CommandEnhancer(initializer, decorator)
+proxy_service = ProxyService()
 
-    echo_template = lambda a, b: f"echo {a} {b}"
-    echo_vars_1 = [1, 2, 3]
-    echo_vars_2 = [4, 5, 6]
-    command_template = CommandTemplate(echo_template, echo_vars_1, echo_vars_2)
+def initialize_driver():
+    global thread_local
+    desired_capabilities = webdriver.DesiredCapabilities.CHROME
+    proxy = Proxy()
+    proxy.proxy_type = ProxyType.MANUAL
+    proxy.http_proxy = proxy.ssl_proxy = proxy_service.generate_proxy()
+    proxy.add_to_capabilities(desired_capabilities)
+    chrome_options = Options()
+    #chrome_options.add_argument("--headless")
+    driver = webdriver.Chrome(desired_capabilities=desired_capabilities, options=chrome_options)
+    setattr(thread_local, 'driver', driver)
+    return driver
 
-    test_workers = 3
-    enhancedExecutor = EnhancedExecutor(command_template, curl_proxy_decorator, test_workers)
-    results = enhancedExecutor.run_commands_on_workers()
-    print(f"Results = {results}\nSuccesses: {sum([r['returncode'] == 0 for r in results])}/{len(results)}")
+def get_driver():
+    driver = getattr(thread_local, 'driver', None)
+    if driver is None:
+        driver = initialize_driver()
+    return driver
+
+def driver_execution(driver, url):
+    driver.get(url)
+    WebDriverWait(driver, 60).until(lambda d: d.execute_script('return document.readyState') == 'complete') 
+    time.sleep(10)
+    currency = driver.find_element_by_css_selector('a#XZ_N-dropdown').text
+
+    # Inspect element + https://www.w3schools.com/cssref/css_selectors.asp
+    import code; code.interact(local={**locals(), **globals()}) ## INTERACT
+
+    tickets = driver.find_elements_by_css_selector('div.resultWrapper')
+    ticket = tickets[0]
+    segments = ticket.find_elements_by_css_selector('div.segment-row')
+    segment = segments[0]
+    date = segment.find_element_by_css_selector('div.date').text
+    time = segment.find_element_by_css_selector('div.segmentTimes').text
+    cabin_class = segment.find_element_by_css_selector('span.segmentCabinClass').text
+    duration = segment.find_element_by_css_selector('span.segmentDuration').text
+    codes = segment.find_element_by_css_selector('div.airport-codes').text
+    plane = segment.find_element_by_css_selector('div.planeDetails').text
+
+    output = str(results)
+    succeedeed = True
+    
+    return { 'output': output, 'succeeded': succeedeed }
+    
+command_context = CommandContext(initialize_driver, get_driver, driver_execution)
+
+###########################################################
+
+def url_template(a): return f"{a}"
+url_vars = ['https://www.kayak.com/flights/JAX-SIN/2020-02-28/2020-03-12/1adults/children-1S-1L?sort=price_a', 'https://www.kayak.com/flights/JAX-SIN/2020-02-28/2020-03-22/1adults/children-1S-1L?sort=price_a']
+
+arg_template = CommandTemplate(url_template, url_vars)
+
+###########################################################
+
+workers = 2
+context_executor = ContextExecutor(arg_template, command_context, workers)
+results = context_executor.run_commands_on_workers()
+print(f"Results = {results}\nSuccesses: {sum([r['returncode'] == 0 for r in results])}/{len(results)}")
+import code; code.interact(local={**locals(), **globals()}) ## INTERACT
